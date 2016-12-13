9b352db01f06b779581c276a4d21fcb7180331c5
Alex Storm <alex6614@edu.easj.dk>
2016-12-13 23:13:26 +0100

    [NEW] Class Reservation [CREATE] Instance, properties, constructor.

---
+++ CommunalEating/Models/Reservation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunalEating.Models
+{
+    class Reservation
+    {
+        private int _dinnerId;
+        private int _noOfAdults;
+        private int _noOfTeens;
+        private int _noOfKids;
+        private int _noOfBabies;
+
+        #region Constructor
+
+        public Reservation(int dinnerId, int noOfAdults, int noOfTeens, int noOfKids, int noOfBabies)
+        {
+            _dinnerId = dinnerId;
+            _noOfAdults = noOfAdults;
+            _noOfTeens = noOfTeens;
+            _noOfKids = noOfKids;
+            _noOfBabies = noOfBabies;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int dinnerId
+        {
+            get { return _dinnerId; }
+            set { _dinnerId = value; }
+        }
+
+        public int noOfAdults
+        {
+            get { return _noOfAdults; }
+            set { _noOfAdults = value; }
+        }
+
+        public int noOfTeens
+        {
+            get { return _noOfTeens; }
+            set { _noOfTeens = value; }
+        }
+
+        public int noOfKids
+        {
+            get { return _noOfKids; }
+            set { _noOfKids = value; }
+        }
+
+        public int noOfBabies
+        {
+            get { return _noOfBabies; }
+            set { _noOfBabies = value; }
+        }
+
+        #endregion
+
+
+
+
+    }
+}
